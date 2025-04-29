892e2211345c26c53b11a291726661815ff96891
shariful8523 <mdsharifulislam8523@gmail.com>
2025-04-29 18:34:14 +0600

    added Lottie Animation in Error page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,13 +1,16 @@
 import { Link } from 'react-router-dom';
+import Lottie from "lottie-react";
+import animationData from "../Animations/Animation - 1745929629681.json";
+
 
 const ErrorPage = () => {
     return (
         <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 p-4">
-            <h1 className="text-6xl font-bold text-red-500 mb-4">404</h1>
-            <h2 className="text-2xl md:text-4xl font-semibold mb-2 text-gray-700">Oops! Page not found.</h2>
-            <p className="text-gray-600 mb-6 text-center max-w-md">
-                The page you are looking for might have been removed, had its name changed, or is temporarily unavailable.
-            </p>
+            <Lottie
+                animationData={animationData}
+                loop={true}        
+                autoplay={true}     
+            />
             <Link to="/" className="bg-blue-500 hover:bg-blue-600 text-white px-6 py-3 rounded-lg transition">
                 Back to Home
             </Link>
